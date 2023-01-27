adaece17c59f15ff7d7790ba790978f5164231af
Erik Na <erikna@Eriks-MacBook-Air.local>
2023-01-27 16:46:55 -0300

    correcao de lazy loading image

--- src/components/AdComponent/AdComponent.tsx
+++ src/components/AdComponent/AdComponent.tsx
@@ -1,11 +1,24 @@
 import React, { useEffect } from 'react'
 
 export const AdComponent = () => {
+    useEffect(() => {
+        const anyWindow = window as any
+
+        try {
+            (anyWindow.adsbygoogle = anyWindow.adsbygoogle || []).push({});
+        } catch (e) {
+            console.log(e)
+        }
+
+    }, []);
+
     return (
-        <ins className='adsbygoogle'
-            style={{ display: 'block' }}
-            data-ad-client='ca-pub-12121212'
-            data-ad-slot='12121212'
-            data-ad-format='auto' />
+        <ins className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-9460540080778149"
+            data-ad-slot=""
+            data-ad-format="auto"
+            data-full-width-responsive="true">
+        </ins>
     )
 }
